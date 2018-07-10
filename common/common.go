145a9d7303fb65f63cd6a0772b2b71c419dc3a41
broadroad <tjbroadroad@163.com>
2018-07-10 21:40:09 +0800

    rm chinese

--- common/common.go
+++ common/common.go
@@ -8,9 +8,9 @@ import (
 type Ticker struct {
 	ContractType string       `json:"omitempty"`
 	Pair         CurrencyPair `json:"omitempty"`
-	Last24Amount float64      `json:last24amount` // 过去24小时成交量
-	LastAmount   float64      `json:lastamount`   // 上一笔成交
-	Last         float64      `json:"last"`       // 最新成交价
+	Last24Amount float64      `json:last24amount` // last 24 hours volume
+	LastAmount   float64      `json:lastamount`   // last deal volume
+	Last         float64      `json:"last"`       // last deal price
 	Buy          float64      `json:"buy"`
 	Sell         float64      `json:"sell"`
 	High         float64      `json:"high"`
