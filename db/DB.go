ffd05778921432de6a740918c5958df0076713a2
Zhang Kang <kang.zhang@emc.com>
2018-07-25 13:08:11 +0800

    add Ticker

--- db/DB.go
+++ db/DB.go
@@ -13,13 +13,14 @@ func Init() (*gorm.DB, error) {
 	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
 		return "prefix_" + defaultTableName
 	}
-	db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/mysql?charset=utf8")
+	db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/mysql?charset=utf8&parseTime=True&loc=Asia/Shanghai")
 	//db, err := gorm.Open("mysql", "root:mysql@/wblog?charset=utf8&parseTime=True&loc=Asia/Shanghai")
 	if err == nil {
 		DB = db
 		//db.LogMode(true)
 		db.DB().SetMaxIdleConns(10)
 		db.DB().SetMaxOpenConns(100)
+		db.CreateTable(&Ticker{})
 		return db, err
 	}
 	return nil, err
--- db/huobiDB.go
+++ db/huobiDB.go
@@ -1,7 +1,24 @@
 package db
 
+import (
+	"time"
+)
+
 // each pair and each time period has a table
-type huobi_ethbtc_ticker_1min struct {
-	ID int `gorm:"primary_key"`
-	//TODO
+// Ticker is common ticker table
+type Ticker struct {
+	ID                 int `gorm:"primary_key"`
+	CreatedAt          time.Time
+	LastDeal           float64 `gorm:"column:last_deal"`
+	LastDealAmount     float64 `gorm "column:last_deal_amount"`
+	HighBuy            float64 `gorm "column:high_buy"`
+	HighBuyAmount      float64 `gorm: "column:high_buy_amount"`
+	LowSell            float64 `gorm: "column:low_sell"`
+	LowSellAmount      float64 `gorm: "column:low_sell_amount"`
+	Last24Price        float64 `gorm: "column:last_24_price"`
+	Last24HighPrice    float64 `gorm: "column:last_24_high_price"`
+	Last24LowPrice     float64 `gorm: "column:last_24_low_price"`
+	Last24BaseAmount   float64 `gorm: "column:last_24_base_amount"`
+	Last24TargetAmount float64 `gorm: "column:last_24_target_amount"`
+	timestamp          time.Time
 }
